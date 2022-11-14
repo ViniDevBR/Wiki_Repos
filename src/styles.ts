f71b6afae0a373ca430afae07f100bbfb4f55da9
ViniDevBR <vinigtr386@gmail.com>
2022-11-14 18:29:44 -0300

    :fire: STyled components

---
+++ src/styles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *, *::after, *::before {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #22272e;
+    color: #ffffff
+  }
+`
